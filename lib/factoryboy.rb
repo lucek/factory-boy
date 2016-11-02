d19c6967c88c2806d33d0d3ac217274671873d72
lucek <lukasz.odziewa@gmail.com>
2016-11-02 22:01:36 +0100

    passing blocks when defining factories

--- lib/factoryboy.rb
+++ lib/factoryboy.rb
@@ -1,16 +1,17 @@
-require "factoryboy/version"
-
 module Factoryboy
-  @defined_factories = []
+  @defined_factories = {}
 
-  def self.define_factory(klass)
-    @defined_factories << klass
+  def self.define_factory(klass, &block)
+    attributes = AttributeBuilder.new(block)
+    @defined_factories[:"#{klass}"] = AttributeBuilder.new(block)
   end
 
   def self.build(klass, attributes={})
-    raise FactoryNotDefinedError unless @defined_factories.include?(klass)
+    klass_symbol = klass.to_s.to_sym
+    raise FactoryNotDefinedError unless @defined_factories.keys.include?(klass_symbol)
 
     object = klass.new
+    @defined_factories[klass_symbol].evaluate(object)
 
     if !attributes.empty?
       attributes.each do |k,v|
@@ -19,6 +20,23 @@ module Factoryboy
     end
 
     return object
+  end
+end
+
+class AttributeBuilder
+  def initialize(block)
+    @block = block
+  end
+
+  def method_missing(method, *args, &block)
+    @object.send("#{method}=", args.first)
+  end
+
+  def evaluate(object)
+    if @block
+      @object = object
+      instance_eval(&@block)
+    end
   end
 end
 
--- spec/factoryboy_spec.rb
+++ spec/factoryboy_spec.rb
@@ -9,35 +9,61 @@ describe Factoryboy do
   describe "#define_factory" do
     it "should define factory for a given class" do
       Factoryboy.define_factory(User)
-      expect(Factoryboy.instance_variable_get(:@defined_factories)).to include(User)
+      expect(Factoryboy.instance_variable_get(:@defined_factories)).to include(:User)
     end
   end
 
   describe "#build" do
     context "factory has already been defined" do
-      before :each do
-        Factoryboy.define_factory(User)
-      end
+      context "user has not passed block when defining factory" do
+        before :each do
+          Factoryboy.define_factory(User)
+        end
 
-      context "user hasn't passed any attributes to the build method" do
-        it "should return new class instance with standard attributes" do
-          object = Factoryboy.build(User)
-          expect(object).to be_a(User)
+        context "user hasn't passed any attributes to the build method" do
+          it "should return new class instance with standard attributes" do
+            object = Factoryboy.build(User)
+            expect(object).to be_a(User)
+          end
+        end
+
+        context "user has passed attributes to the build method" do
+          it "should return new class instance with passed attributes" do
+            object = Factoryboy.build(User, { name: "foo" })
+            expect(object).to be_a(User)
+            expect(object.name).to eql("foo")
+          end
         end
       end
 
-      context "user has passed attributes to the build method" do
-        it "should return new class instance with passed attributes" do
-          object = Factoryboy.build(User, { name: "foo" })
-          expect(object).to be_a(User)
-          expect(object.name).to eql("foo")
+      context "user has passed block when defining factory" do
+        before :each do
+          Factoryboy.define_factory(User) do
+            name "bar"
+          end
+        end
+
+        context "user hasn't passed any attributes to the build method" do
+          it "should return new class instance with passed attributes" do
+            object = Factoryboy.build(User)
+            expect(object).to be_a(User)
+            expect(object.name).to eql("bar")
+          end
+        end
+
+        context "user has passed attributes to the build method" do
+          it "should return new class instance with passed attributes" do
+            object = Factoryboy.build(User, { name: "foo" })
+            expect(object).to be_a(User)
+            expect(object.name).to eql("foo")
+          end
         end
       end
     end
 
     context "factory has not been defined yet" do
       before :each do
-        Factoryboy.instance_variable_set(:@defined_factories, [])
+        Factoryboy.instance_variable_set(:@defined_factories, {})
       end
 
       it "should raise FactoryNotDefined" do
